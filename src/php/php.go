b52d439e4d6886e83860c62e0c31f16eb3a1ff94
Kerem Gunes <qeremy@gmail.com>
2016-01-05 18:13:06 +0200

    add php.

---
+++ src/php/php.go
@@ -0,0 +1,50 @@
+package php
+
+import (
+    _fmt "fmt"
+)
+
+func Shutup() {}
+
+// Dump.
+//
+// @param  args... interface{}
+// @return (void)
+func Dump(args... interface{}) {
+    _fmt.Println(args...)
+}
+
+// Dump as string.
+//
+// @param  args... interface{}
+// @return (void)
+func Dumps(args... interface{}) {
+    var f string
+    for _, arg := range args {
+        _ = arg // silence..
+        f += "%+v "
+    }
+    _fmt.Printf("%s\n", _fmt.Sprintf(f, args...))
+}
+
+// Dump types.
+//
+// @param  args... interface{}
+// @return (void)
+func Dumpt(args... interface{}) {
+    var f string
+    for _, arg := range args {
+        _ = arg // silence..
+        f += "%T "
+    }
+    _fmt.Printf("%s\n", _fmt.Sprintf(f, args...))
+}
+
+// Dump as formatted string.
+//
+// @param  f       string
+// @param  args... interface{}
+// @return (void)
+func Dumpf(f string, args... interface{}) {
+    _fmt.Printf("%s\n", _fmt.Sprintf(f, args...))
+}
