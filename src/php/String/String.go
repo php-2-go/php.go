12841049245abc326c2fd4b26ba0d70cf73debe1
Kerem Gunes <qeremy@gmail.com>
2016-01-05 17:43:46 +0200

    add Sub().

--- src/php/String/String.go
+++ src/php/String/String.go
@@ -1,5 +1,9 @@
 package String
 
+import (
+    _n "php/Integer"
+)
+
 // func Len(s string) (uint) {}
 
 // func Pos(s, ss string) (int, bool) {}
@@ -13,4 +17,40 @@ func Rev(s string) (string) {
         rr[ii] = sr[i]
     }
     return string(rr)
+}
+
+func Sub(s string, ss, sl interface{}) (string) {
+    rs := []rune(s)
+    rl := len(rs)
+    ssi, sli := _n.Int(ss), _n.Int(sl)
+    ssl, sll := ssi, sli
+    // make abs
+    if ssi <= -1 { ssl = (ssi * -1) }
+    if sli <= -1 { sll = (sli * -1) }
+    // out of range returns ""
+    if ssl >= rl { return "" }
+    if sll >= rl { return "" }
+    if sl == nil {
+        if ssi <= -1 {
+            rr := []rune(Rev(s))
+            if ssl >= len(rr) {
+                return s
+            }
+            var rs string
+            for i := 0; i < ssl; i++ {
+                rs = string(rr[i]) + rs
+            }
+            return rs
+        }
+        return string(rs[ssl:])
+    }
+    if sli >= 1 {
+        // prevent "out of range" error
+        if sll > rl {
+            return string(rs[ssl: rl])
+        }
+        return string(rs[ssl: ssl + sll])
+    } else {
+        return string(rs[ssl: len(rs) - sll])
+    }
 }
