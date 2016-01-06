92262b24f80f9792174b9dd02a68246bad848477
Kerem Gunes <qeremy@gmail.com>
2016-01-06 14:01:03 +0200

    add php_conv.go

---
+++ src/php/php_conv.go
@@ -0,0 +1,16 @@
+package php
+
+import (
+    _n "php/Number"
+)
+
+// Int converter.
+//
+// @param  i interface{}
+// @return (int)
+func Int(i interface{}) (int) {
+    if n := _n.Convert(i, "int"); n != nil {
+        return n.(int)
+    }
+    return 0
+}
