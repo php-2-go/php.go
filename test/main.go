5b80c3262ddd4994cc1834311c4687c5dbf683eb
Kerem Gunes <qeremy@gmail.com>
2016-01-06 15:55:52 +0200

    tests..

--- test/main.go
+++ test/main.go
@@ -2,30 +2,35 @@ package main
 
 import (
     "php"
-    _u "php/Url"
+    // _u "php/Url"
     // _s "php/String"
 )
 
 func main() {
-    x := _u.QueryParse("a=1&b")
-    php.Dumpa(x)
-    y := _u.QueryUnparse(x)
-    php.Dumps(y)
+    // x := "1.1"
+    // php.Dumpa(php.Int(x))
+    // php.Dumpa(php.Float(x))
+    // php.Dumpa(php.Bool(x))
+    // php.Dumpa(php.String(x))
 
-    // s:= "0abcçdef"
-    // php.Dump(_s.Pos(s, "ç"))
-    // php.Dump(_s.Posi(s, "Ç"))
-    // php.Dump(_s.Posr(s, "ç"))
-    // php.Dump(_s.Posri(s, "Ç"))
+    // s:= "0açe"
+    // php.Dump(_s.Index(s, "ç"))
+    // php.Dump(_s.Index(s, "ç"))
+    // php.Dump(_s.IndexNC(s, "Ç"))
+    // php.Dump(_s.IndexLast(s, "ç"))
+    // php.Dump(_s.IndexLastNC(s, "Ç"))
 
-    // php.Dump(_s.Len(s))
-    // php.Dump(_s.Rev(s))
+    // php.Dump(_s.Length(s))
+    // php.Dump(_s.Reverse(s))
 
-    // php.Dumpf("%#v", _s.Sub(s, -1, nil))
-    // php.Dumpf("%#v", _s.Sub(s, -2, nil))
-    // php.Dumpf("%#v", _s.Sub(s, -3, 1))
-    // php.Dumpf("%#v", _s.Sub(s, 1, nil))
-    // php.Dumpf("%#v", _s.Sub(s, 1, 1))
-    // php.Dumpf("%#v", _s.Sub(s, 6, 2))
-    // php.Dumpf("%#v", _s.Sub(s, 1, -6))
+    // php.Dump(_s.Ord("♥"))
+    // php.Dump(_s.Chr(_s.Ord("♥")))
+
+    // php.Dumpf("%#v", _s.Substring(s, -1, nil))
+    // php.Dumpf("%#v", _s.Substring(s, -2, nil))
+    // php.Dumpf("%#v", _s.Substring(s, -3, 1))
+    // php.Dumpf("%#v", _s.Substring(s, 1, nil))
+    // php.Dumpf("%#v", _s.Substring(s, 1, 1))
+    // php.Dumpf("%#v", _s.Substring(s, 6, 2))
+    // php.Dumpf("%#v", _s.Substring(s, 1, -6))
 }
