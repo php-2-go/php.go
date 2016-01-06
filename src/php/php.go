2c743c425070d13fbb425ff2c25ebbda752e2cdd
Kerem Gunes <qeremy@gmail.com>
2016-01-06 14:33:01 +0200

    add php_dump.go, move related func's.

--- src/php/php.go
+++ src/php/php.go
@@ -7,62 +7,6 @@ import (
 
 func Shutup() {}
 
-// Dump.
-//
-// @param  args... interface{}
-// @return (void)
-func Dump(args... interface{}) {
-    _fmt.Println(args...)
-}
-
-// Dump as string.
-//
-// @param  args... interface{}
-// @return (void)
-func Dumps(args... interface{}) {
-    var f string
-    for _, arg := range args {
-        _ = arg // silence..
-        f += "%+v "
-    }
-    _fmt.Printf("%s\n", _fmt.Sprintf(f, args...))
-}
-
-// Dump types.
-//
-// @param  args... interface{}
-// @return (void)
-func Dumpt(args... interface{}) {
-    var f string
-    for _, arg := range args {
-        _ = arg // silence..
-        f += "%T "
-    }
-    _fmt.Printf("%s\n", _fmt.Sprintf(f, args...))
-}
-
-// Dump all.
-//
-// @param  args... interface{}
-// @return (void)
-func Dumpa(args... interface{}) {
-    var f string
-    for _, arg := range args {
-        _ = arg // silence..
-        f += "%#v "
-    }
-    _fmt.Printf("%s\n", _fmt.Sprintf(f, args...))
-}
-
-// Dump as formatted string.
-//
-// @param  f       string
-// @param  args... interface{}
-// @return (void)
-func Dumpf(f string, args... interface{}) {
-    _fmt.Printf("%s\n", _fmt.Sprintf(f, args...))
-}
-
 // Get real type.
 //
 // @param  args.. interface{}
---
+++ src/php/php_dump.go
@@ -0,0 +1,57 @@
+package php
+
+// Dump.
+//
+// @param  args... interface{}
+// @return (void)
+func Dump(args... interface{}) {
+    _fmt.Println(args...)
+}
+
+// Dump as string.
+//
+// @param  args... interface{}
+// @return (void)
+func Dumps(args... interface{}) {
+    var f string
+    for _, arg := range args {
+        _ = arg // silence..
+        f += "%+v "
+    }
+    _fmt.Printf("%s\n", _fmt.Sprintf(f, args...))
+}
+
+// Dump types.
+//
+// @param  args... interface{}
+// @return (void)
+func Dumpt(args... interface{}) {
+    var f string
+    for _, arg := range args {
+        _ = arg // silence..
+        f += "%T "
+    }
+    _fmt.Printf("%s\n", _fmt.Sprintf(f, args...))
+}
+
+// Dump all.
+//
+// @param  args... interface{}
+// @return (void)
+func Dumpa(args... interface{}) {
+    var f string
+    for _, arg := range args {
+        _ = arg // silence..
+        f += "%#v "
+    }
+    _fmt.Printf("%s\n", _fmt.Sprintf(f, args...))
+}
+
+// Dump as formatted string.
+//
+// @param  f       string
+// @param  args... interface{}
+// @return (void)
+func Dumpf(f string, args... interface{}) {
+    _fmt.Printf("%s\n", _fmt.Sprintf(f, args...))
+}
