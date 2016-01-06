ba716ab474a25ffc4e42fddd5eeca4762cbcc3a6
Kerem Gunes <qeremy@gmail.com>
2016-01-06 14:25:22 +0200

    add Float(), modify Int().

--- src/php/php_conv.go
+++ src/php/php_conv.go
@@ -8,11 +8,23 @@ import (
 // Int converter.
 //
 // @param  i interface{}
-// @return (int)
-func Int(i interface{}) (int) {
-    r, err := _strc.Atoi(_fmt.Sprintf("%v", i))
+// @return (int64)
+func Int(i interface{}) (int64) {
+    r, err := _strc.ParseInt(_fmt.Sprintf("%v", i), 10, 32)
     if err == nil {
-        return r
+        return (r)
     }
     return 0
+}
+
+// Float converter.
+//
+// @param  i interface{}
+// @return (float64)
+func Float(i interface{}) (float64) {
+    r, err := _strc.ParseFloat(_fmt.Sprintf("%v", i), 64)
+    if err == nil {
+        return float64(r)
+    }
+    return 0.00
 }
