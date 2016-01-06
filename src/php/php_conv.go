bef14e9142ebd5bff63743b36b93e51a00b47a13
Kerem Gunes <qeremy@gmail.com>
2016-01-06 14:26:52 +0200

    add String().

--- src/php/php_conv.go
+++ src/php/php_conv.go
@@ -10,7 +10,7 @@ import (
 // @param  i interface{}
 // @return (int64)
 func Int(i interface{}) (int64) {
-    r, err := _strc.ParseInt(_fmt.Sprintf("%v", i), 10, 32)
+    r, err := _strc.ParseInt(String(i), 10, 32)
     if err == nil {
         return (r)
     }
@@ -22,9 +22,17 @@ func Int(i interface{}) (int64) {
 // @param  i interface{}
 // @return (float64)
 func Float(i interface{}) (float64) {
-    r, err := _strc.ParseFloat(_fmt.Sprintf("%v", i), 64)
+    r, err := _strc.ParseFloat(String(i), 64)
     if err == nil {
         return float64(r)
     }
     return 0.00
+}
+
+// String converter.
+//
+// @param  i interface{}
+// @return (string)
+func String(i interface{}) (string) {
+    return _fmt.Sprintf("%v", i)
 }
